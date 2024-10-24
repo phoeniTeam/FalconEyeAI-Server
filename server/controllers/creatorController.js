7284d3f64483771505b748f495a7b2e242394c92
Rana Almalki <ranaalmalki12@gmail.com>
2024-10-24 10:57:52 +0300

    add the validationRules for the register

--- server/controllers/creatorController.js
+++ server/controllers/creatorController.js
@@ -1,8 +1,24 @@
 import Creator from '../models/creatorSchema.js';
-import { validationResult } from 'express-validator';
+import { body, validationResult } from 'express-validator';
 
+
+//
+const validatorRules = [
+body('name').notEmpty().withMessage('Name is required'),
+body('username').notEmpty().withMessage('Username is required'),
+body('email').custom(async value => {
+    const user = await Creator.findOne({ email: value });
+    if (user) {
+      throw new Error('E-mail already in use');
+    }
+  }),body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
+
+]
 // sign-up new creator account
-export const createCreator = async (req, res) => {
+export const createCreator = 
+[
+    ...validatorRules,
+async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
@@ -27,4 +43,4 @@ export const createCreator = async (req, res) => {
 }
 
 
-
+]
