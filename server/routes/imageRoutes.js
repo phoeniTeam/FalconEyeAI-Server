bb248503a7017633b1c80a9bcefe1bfbf02f7113
comsaziz <comsaziz2@gmail.com>
2024-10-25 16:40:37 +0300

    Add authMiddleware to the imageRoutes in the server

--- server/routes/imageRoutes.js
+++ server/routes/imageRoutes.js
@@ -1,13 +1,15 @@
 import express from "express";
 import { createImage, deleteImage, getAllImages, getImageById, patchImage, updateImage } from "../controllers/imageController.js";
+import { imageValidationRules } from "../validators/imageValidator.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/", createImage);
+router.post("/", authMiddleware, imageValidationRules(), createImage);
 router.get("/", getAllImages);
-router.get("/:id", getImageById);
-router.put("/:id", updateImage);
-router.patch("/:id", patchImage);
-router.delete("/:id", deleteImage);
+router.get("/:id", authMiddleware, getImageById);
+router.put("/:id", authMiddleware, imageValidationRules(), updateImage);
+router.patch("/:id", authMiddleware, imageValidationRules(), patchImage);
+router.delete("/:id", authMiddleware, deleteImage);
 
 export default router;
--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ import cors from "cors";
 import connectDB from "./config/database.js";
 import creatorRoutes from "./routes/creatorRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
+import imageRoutes from "./routes/imageRoutes.js";
+import transactionRoutes from "./routes/transactionRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 8000;
@@ -16,9 +18,11 @@ connectDB();
 
 app.use("/api/auth", authRoutes);
 app.use("/creators", creatorRoutes);
+app.use("/images", imageRoutes);
 app.use("/transactions", transactionRoutes);
 
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
