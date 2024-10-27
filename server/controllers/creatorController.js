41830ab807e57b6c57fd6887d5c486c1004373a1
comsaziz <comsaziz2@gmail.com>
2024-10-27 22:39:39 +0300

    Fix transaction bug in Creator

--- server/controllers/creatorController.js
+++ server/controllers/creatorController.js
@@ -1,80 +1,13 @@
 import Creator from "../models/creatorSchema.js";
-import { body, validationResult } from "express-validator";
+import { validationResult } from "express-validator";
 
-//
-// const validatorRules = [
-// body('name').notEmpty().withMessage('Name is required'),
-// body('username').notEmpty().withMessage('Username is required'),
-// body('email').custom(async value => {
-//     const user = await Creator.findOne({ email: value });
-//     if (user) {
-//       throw new Error('E-mail already in use');
-//     }
-//   }),body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
-
-// ]
-// // sign-up new creator account
-// export const createCreator =
-// [
-//     ...validatorRules,
-// async (req, res) => {
-//     const errors = validationResult(req);
-//     if (!errors.isEmpty()) {
-//         return res.status(400).json({ errors: errors.array() });
-//     }
-//     try{
-//         const newCreator = new Creator({
-//             name: req.body.name,
-//             username:req.body.username,
-//             email:req.body.email,
-//             password:req.body.password,
-//         })
-//         await newCreator.save();
-//         res.status(201).json(newCreator);
-
-//     } catch (error) {
-//         console.error(error.message);
-//         res.status(500).send('Server Error');
-//     }
-
-// }
-
-// ]
-
-// // log in creator account
-// export const loginCreator = async (req, res) => {
-//     try{
-//         const loginCreatorAccount = await Creator.findOne(
-//             { email: req.body.email,
-//                 password: req.body.password
-
-//              });
-//              if(!loginCreatorAccount){
-//                  return res.status(400).json({msg: 'Invalid email address'});
-//              }
-
-//              if(req.body.password !== loginCreatorAccount.password){
-//                  return res.status(400).json({msg: "invalid password"});
-//              }
-//              res.status(200).json({msg: 'Logged in successfully' ,creator: loginCreatorAccount});
-
-//     }catch (error) {
-//         console.error(error.message);
-//         res.status(500).send('Server Error');
-//     }
-
-// }
-
-//get all creators data
-
+// Get all creators
 export const getAllCreators = async (req, res) => {
   try {
-    const creators = await Creator.find();
-
+    const creators = await Creator.find().populate('images').populate('transactions');
     if (creators.length === 0) {
       return res.status(404).json({ msg: "No creators found" });
     }
-
     return res.status(200).json(creators);
   } catch (error) {
     console.error(error.message);
@@ -82,58 +15,91 @@ export const getAllCreators = async (req, res) => {
   }
 };
 
-
-//get creator  by id
+// Get a creator by ID
 export const getCreatorById = async (req, res) => {
   const id = req.params.id;
   try {
-    const creator = await Creator.findById(id);
+    const creator = await Creator.findById(id).populate('images').populate('transactions');
     if (!creator) {
       return res.status(404).json({ msg: "Creator not found" });
     }
-    res.send(creator);
+    res.json(creator);
   } catch (error) {
     console.error(error.message);
     res.status(500).send("Server Error");
   }
 };
 
-// update creator  by id
-export const updateCreator = async (req, res) => {
-  const id = req.params.id;
-  try{
-    const updatedCreator = req.body;
-    const updatedCreatorAccount = await Creator.findByIdAndUpdate(
-      id,
-      updatedCreator,
-      { new: true }
-    );
-    if (!updatedCreatorAccount) {
-      return res.status(404).json({ msg: "Creator not found" });
-    }
-    res.status(200).send(updatedCreatorAccount);;
+// Create a new creator
+export const createCreator = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 
-  }catch (error) {
+  try {
+    const { name, username, email, password, photo, planId, images, transactions, creditBalance } = req.body;
+    const newCreator = new Creator({
+      name,
+      username,
+      email,
+      password,
+      photo,
+      planId,
+      images,
+      transactions,
+      creditBalance
+    });
+
+    await newCreator.save();
+    res.status(201).json(newCreator);
+  } catch (error) {
     console.error(error.message);
     res.status(500).send("Server Error");
   }
- 
 };
 
-
-// delete creator by id
-export const deleteCreatorById = async (req, res) => {
-    const id = req.params.id;
-    try{
-        const deletedCreator = await Creator.findByIdAndDelete(id);
-        if (!deletedCreator) {
-            return res.status(404).json({ msg: "Creator not found" });
-        }
-        res.status(200).send(deletedCreator);;
-
-
-    }catch (error) {
-        console.error(error.message);
-        res.status(500).send('Server Error');
+// Update creator by ID
+export const updateCreator = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const updatedCreator = await Creator.findByIdAndUpdate(id, req.body, { new: true });
+    if (!updatedCreator) {
+      return res.status(404).json({ msg: "Creator not found" });
     }
-}
+    res.status(200).json(updatedCreator);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+};
+
+// Partially update creator by ID
+export const patchCreator = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const patchedCreator = await Creator.findByIdAndUpdate(id, req.body, { new: true });
+    if (!patchedCreator) {
+      return res.status(404).json({ msg: "Creator not found" });
+    }
+    res.status(200).json(patchedCreator);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+};
+
+// Delete creator by ID
+export const deleteCreatorById = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const deletedCreator = await Creator.findByIdAndDelete(id);
+    if (!deletedCreator) {
+      return res.status(404).json({ msg: "Creator not found" });
+    }
+    res.status(200).json({ msg: "Creator deleted successfully", deletedCreator });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+};
--- server/routes/creatorRoutes.js
+++ server/routes/creatorRoutes.js
@@ -1,14 +1,41 @@
 import express from 'express';
-import {  deleteCreatorById, getAllCreators, getCreatorById, updateCreator } from '../controllers/creatorController.js';
-
+import {  deleteCreatorById, getAllCreators, getCreatorById, updateCreator, createCreator,patchCreator } from '../controllers/creatorController.js';
+import { creatorValidationRules } from '../Validators/creatorValidator.js';
+import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const router =express.Router();
 
-// router.post('/',createCreator)
-// router.get('/', loginCreator )
+
 router.get('/', getAllCreators)
-router.get('/:id', getCreatorById)
-router.put('/:id', updateCreator)
-router.delete('/:id', deleteCreatorById)
+router.get('/:id', authMiddleware ,getCreatorById)
+router.post('/', authMiddleware, creatorValidationRules(), createCreator)  
+router.put('/:id', authMiddleware, creatorValidationRules(), updateCreator)
+router.patch('/:id', authMiddleware, patchCreator)  
+router.delete('/:id', authMiddleware, deleteCreatorById)
 
 export default router;
+
+
+
+// import express from "express";
+// import { creatorValidationRules } from '../Validators/creatorValidator.js';
+// import { authMiddleware } from "../middlewares/authMiddleware.js";
+// import {
+//   createCreator,
+//   deleteCreatorById,
+//   getAllCreators,
+//   getCreatorById,
+//   patchCreator,
+//   updateCreator,
+// } from "../controllers/creatorController.js";
+
+// const router = express.Router();
+
+// router.get("/", getAllCreators);
+// router.get("/:id", getCreatorById); // Adjust authMiddleware if needed
+// router.post("/", authMiddleware, creatorValidationRules, createCreator);
+// router.put("/:id", authMiddleware, creatorValidationRules, updateCreator);
+// router.patch("/:id", authMiddleware, patchCreator);
+// router.delete("/:id", authMiddleware, deleteCreatorById);
+
+// export default router;
