a3e54a9ec286b6d9ffaa4c5c5055e697b161cfdc
mohammed-dhabaab <mohammed.dhabaab@gmail.com>
2024-10-29 22:55:17 +0300

    Fixed a bug

--- server/Validators/creatorValidator.js
+++ server/Validators/creatorValidator.js
@@ -2,8 +2,8 @@ import { body } from 'express-validator';
 
 const creatorValidator = () => {
     return [
-        body('email').isEmail().withMessage('Please provide a valid email'),
         body('username').isLength({ min: 3 }).withMessage('Username must be at least 3 characters long'),
+        body('email').isEmail().withMessage('Please provide a valid email'),
         body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
         body('name').not().isEmpty().withMessage('Name is required'),
     ];
