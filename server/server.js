12103a53370cf4baac447a972088ddc1649a4c60
comsaziz <comsaziz2@gmail.com>
2024-10-30 21:40:36 +0300

    Test the Render to check

--- server/server.js
+++ server/server.js
@@ -8,17 +8,22 @@ import creatorRoutes from "./routes/creatorRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import imageRoutes from "./routes/imageRoutes.js";
 import transactionRoutes from "./routes/transactionRoutes.js";
-import Stripe from 'stripe';
-import bodyParser from 'body-parser';
+import Stripe from "stripe";
+import bodyParser from "body-parser";
 import { createTransaction } from "./controllers/transactionController.js";
 
-
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8000; 
 app.use(express.json());
-app.use(cors());
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL, 
+  })
+);
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 connectDB();
 
 app.use("/api/auth", authRoutes);
@@ -33,54 +38,56 @@ const checkoutSession = async (req, res) => {
       1: { id: 1, price: 0, name: "Free", credits: "10" },
       2: { id: 2, price: 29, name: "Pro", credits: "220" },
       3: { id: 3, price: 79, name: "Premium", credits: "510" },
-    }
+    };
     const amount = Number(plansDetails[plan].price) * 100;
     const session = await stripe.checkout.sessions.create({
       line_items: [
         {
           price_data: {
-            currency: 'usd',
+            currency: "usd",
             product_data: {
               name: plansDetails[plan].name,
-              description: `Credits ${plansDetails[plan].credits}`
+              description: `Credits ${plansDetails[plan].credits}`,
             },
-            unit_amount: amount
+            unit_amount: amount,
           },
-          quantity: 1
-        }
+          quantity: 1,
+        },
       ],
       metadata: {
         creatorId: req.body.creatorId,
         credits: plansDetails[plan].credits,
-        plan: plansDetails[plan].id
+        plan: plansDetails[plan].id,
       },
-      mode: 'payment',
+      mode: "payment",
       success_url: `${process.env.CLIENT_URL}profile`,
-      cancel_url: `${process.env.CLIENT_URL}`,
+      cancel_url: `${process.env.CLIENT_URL}credit`,
     });
 
     res.json({ id: session.id });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
-app.use("/create-checkout-session", checkoutSession);
 
+app.post("/create-checkout-session", checkoutSession);
 
 const linkStripeWebhook = async (req, res) => {
   let event;
   try {
-    event = stripe.webhooks.constructEvent(req.body, req.headers['stripe-signature'], process.env.STRIPE_WEBHOOK_SECRET);
+    event = stripe.webhooks.constructEvent(
+      req.body,
+      req.headers["stripe-signature"],
+      process.env.STRIPE_WEBHOOK_SECRET
+    );
   } catch (err) {
     console.error(`⚠️  Webhook signature verification failed.`, err.message);
     return res.status(400).send(`Webhook Error: ${err.message}`);
   }
 
-
-  if (event.type === 'checkout.session.completed') {
+  if (event.type === "payment_intent.succeeded") {
     const session = event.data.object;
-
 
     const creatorId = session.metadata.creatorId;
     const credits = session.metadata.credits;
@@ -89,23 +96,23 @@ const linkStripeWebhook = async (req, res) => {
     const amount = session.amount_total;
 
     try {
-
       await createTransaction(stripeId, amount, plan, credits, creatorId);
-
-
-      res.status(200).send('Success');
+      res.status(200).send("Success");
     } catch (error) {
-      console.error('Error creating transaction:', error);
-      res.status(500).send('Internal Server Error');
+      console.error("Error creating transaction:", error);
+      res.status(500).send("Internal Server Error");
     }
   } else {
-
-    res.status(200).send('Received unhandled event');
+    res.status(200).send("Received unhandled event");
   }
-}
-app.post('/stripe', bodyParser.raw({ type: 'application/json' }), linkStripeWebhook);
+};
 
+app.post(
+  "/stripe",
+  bodyParser.raw({ type: "application/json" }),
+  linkStripeWebhook
+);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server is listening on port ${port}`);
 });
